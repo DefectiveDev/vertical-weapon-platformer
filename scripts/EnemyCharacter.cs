a9cd8030e60c2cea13c8da1f94a09d62af7ef3bd
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-09 18:48:43 -0500

    enemy autoamatically moves left and right randomly

--- scripts/EnemyCharacter.cs
+++ scripts/EnemyCharacter.cs
@@ -5,34 +5,36 @@ public partial class EnemyCharacter : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	private Vector2 direction = Vector2.Left;
+	private RayCast2D rayCast2DLeft;
+	private RayCast2D rayCast2DRight;
+
+    public override void _Ready()
+    {
+        //randomly assign direction on startup
+        if (GD.Randf() > 0.5f)
+        {
+            direction = Vector2.Right;
+        }
+
+        rayCast2DLeft = GetNode<RayCast2D>("RayCastLeft");
+        rayCast2DRight = GetNode<RayCast2D>("RayCastRight");
+    }
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+        if (rayCast2DLeft.IsColliding())
+        {
+            direction = Vector2.Right;
+        }
+        else if (rayCast2DRight.IsColliding())
+        {
+            direction = Vector2.Left;
+        }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+        velocity.X = direction.X * Speed;
 
 		Velocity = velocity;
 		MoveAndSlide();
