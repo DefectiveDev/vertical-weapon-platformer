ed45ae908c87ba3a551870059dde01dfd1c956c2
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-10 07:27:46 -0500

    made bullet move based on rotation

--- scripts/Bullet.cs
+++ scripts/Bullet.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class Bullet : Area2D
 {
+    [Export]
+    private float Speed = 100f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Translate(Vector2.Right.Rotated(Rotation) * Speed);
+    }
 }
