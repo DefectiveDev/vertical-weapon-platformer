8c0d39525516aaea6ea5a963adad6a12b9d3e290
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-17 18:40:09 -0500

    Feature: Deathzone is by nearing the edge of the screen instead of at the bottom of the level

--- scripts/KillPlayerZone.cs
+++ scripts/KillPlayerZone.cs
@@ -1,16 +1,29 @@
 using Godot;
-using System;
-
 
 public partial class KillPlayerZone : Node
 {
     [Export]
     private Player player;
-    private static readonly float DeathZoneY = 20;
+    [Export]
+    private float DeathZoneY = 20;
+    private float curHighestBottomScreenPos;
+    private float BottomOfScreenPos => GetViewport().GetVisibleRect().GetCenter().Y + GetViewport().GetCamera2D().GetScreenCenterPosition().Y;
 
-    public override void _Process(double delta)
+    public override void _Ready()
     {
-        if (player.Position.Y > GetViewport().GetVisibleRect().End.Y - DeathZoneY)
+        curHighestBottomScreenPos = BottomOfScreenPos;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        // Y is calculated as negative when going up in 2D Space
+        if (curHighestBottomScreenPos > BottomOfScreenPos)
+        {
+            curHighestBottomScreenPos = BottomOfScreenPos;
+            GetViewport().GetCamera2D().LimitBottom = (int)(curHighestBottomScreenPos + DeathZoneY);
+        }
+
+        if (player.Position.Y > curHighestBottomScreenPos + DeathZoneY)
         {
             GetTree().CallDeferred(SceneTree.MethodName.ReloadCurrentScene);
         }
